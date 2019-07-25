2e5ecd5f29f7379915e2bba18c499da2fb4f4720
Elijah Oyekunle <eloyekunle@gmail.com>
2019-07-25 11:33:46 +0100

    add script section

--- scripts/build-deploy.sh
+++ scripts/build-deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-docker build -t playmice/docker-with-compose-awscli .
-
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker push playmice/docker-with-compose-awscli
